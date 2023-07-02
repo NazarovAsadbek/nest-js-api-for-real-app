c46660f2ccb3af363a428906e6f39af38203ff5c
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2023-07-02 18:25:40 +0500

    [QA] Auth middleware is done! [Added get user by authorization token]

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,18 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
+  Req,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserResponseInterface } from '@app/user/types/userResponse.interface';
-import { LoginUserDto } from "@app/user/dto/login.dto";
+import { LoginUserDto } from '@app/user/dto/login.dto';
+import { Request } from 'express';
+import { ExpressRequestInterface } from '@app/types/expressRequest.interface';
 
 @Controller()
 export class UserController {
@@ -30,5 +34,12 @@ export class UserController {
   ): Promise<UserResponseInterface> {
     const user = await this.userService.login(loginUserDto);
     return this.userService.buildUserResponse(user);
+  }
+
+  @Get('user')
+  async currentUser(
+    @Req() request: ExpressRequestInterface,
+  ): Promise<UserResponseInterface> {
+    return this.userService.buildUserResponse(request.user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -65,6 +65,10 @@ export class UserService {
     return user;
   }
 
+  async findById(id: number): Promise<UserEntity> {
+    return this.userRepository.findOne(id);
+  }
+
   generateJwt(user: UserEntity): string {
     return sign(
       {
