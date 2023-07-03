82a5097562b880d2d800817d1c1c24468a76709e
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2023-07-03 20:33:23 +0500

    [QA] Articles is done;

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
 import { UserModule } from '@app/user/user.module';
+import { ArticleModule } from '@app/article/article.module';
 import ormconfig from '@app/ormconfig';
 import { AuthMiddleware } from '@app/user/middlewares/auth.middleware';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    TagModule,
+    UserModule,
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
