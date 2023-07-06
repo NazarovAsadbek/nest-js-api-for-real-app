2d1489207222a83f3a80294160482f27590f3ea5
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2023-07-06 20:01:37 +0500

    [QA] Update docker-compose

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 if (process.env.IS_TS_NODE) {
   require('module-alias/register');
 }
@@ -7,6 +9,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Realworld API')
+    .setDescription('My pet project for learning nest.js and postgres')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/swagger/index.html', app, document);
+
   await app.listen(3003);
 }
 bootstrap();
