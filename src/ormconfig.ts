edbdefbc77c7413d7a7ba32e1207b0f9ef7d4c18
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2023-07-06 18:59:55 +0500

    [QA] Update docker-compose

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,11 +2,12 @@ import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'postgres',
   port: 5432,
   username: 'mediumclone',
   password: '12345!!!',
   database: 'mediumclone',
+  url: 'postgres://mediumclone:12345!!!@postgres:5432/db',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.ts, .js}'],
